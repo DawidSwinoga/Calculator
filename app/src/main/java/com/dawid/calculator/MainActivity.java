2f7fe265ca52f38a8a62182a11a7766de1d0ecc3
Dawid <dawid.swinoga@sdqac.rd.tp.pl>
2017-03-16 01:25:50 +0100

    Initial commit

---
+++ app/src/main/java/com/dawid/calculator/MainActivity.java
@@ -0,0 +1,30 @@
+package com.dawid.calculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(value = R.id.simple_activity_button)
+    void onSimpleActivityClicked() {
+        Intent intent = new Intent(this, SimpleCalculatorActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(value = R.id.advanced_activity_button)
+    void onAdvancedActivityClicked() {
+        Intent intent = new Intent(this, AdvancedCalculatorActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/dawid/calculator/SimpleCalculatorActivity.java
@@ -0,0 +1,44 @@
+package com.dawid.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayout;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SimpleCalculatorActivity extends AppCompatActivity {
+
+    @BindView(R.id.number_pad_grid)
+    GridLayout numberPadGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_calculator);
+        ButterKnife.bind(this);
+        initNumberPadGrid();
+    }
+
+    private void initNumberPadGrid() {
+        int[] numbers = {7,8,9,4,5,6,1,2,3};
+
+        for(final int number : numbers) {
+            Button numberPadButton = new Button(this);
+            numberPadButton.setText(String.valueOf(number));
+            numberPadButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println(number);
+                }
+            });
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+            params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+            numberPadButton.setLayoutParams(params);
+            numberPadGrid.addView(numberPadButton);
+        }
+    }
+}
